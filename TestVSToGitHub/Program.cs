01771a9fec488da9f54443f4f299a867143c2b1b
aliya <aliya@ALIYASLAPTOP>
2024-10-03 08:58:37 -0500

    Add project files.

---
+++ TestVSToGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestVSToGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
